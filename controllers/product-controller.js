010df76d0dc109119b3c5343147f7cd4066396fb
gemr <gemrdev@gmail.com>
2024-06-21 22:32:04 -0500

    updates

--- controllers/product-controller.js
+++ controllers/product-controller.js
@@ -2,9 +2,22 @@ import 'dotenv/config'
 import Product from '../models/product-model.js'
 
 export async function createProduct(request, response) {
-    // code here...
+    try {
+        const newProduct = new Product(request.body);
+        await newProduct.save();
+        response.status(200).json({ message: 'Product created' });
+    } catch (error) {
+        console.error(error);
+        response.status(500).json({ error: 'Internal error, failed to create product' });
+    }
 }
 
 export async function getAllProducts(request, response) {
-    // code here...
+    try {
+        const products = await Product.find(null, 'name description price');
+        response.status(200).json(products);
+    } catch (error) {
+        console.error(error);
+        response.status(500).json({ error: 'Internal error, failed to get all products' });
+    }
 }
